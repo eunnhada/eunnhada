e612835bd80264dcabcd48e731dc094e2647568b
eunnhada <kimhuya0605@gmail.com>
2026-03-23 14:03:11 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
+
+if (menuToggle && mainNav) {
+  menuToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("open");
+  });
+}
+
+const navLinks = document.querySelectorAll(".main-nav a");
+const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+navLinks.forEach((link) => {
+  const linkPage = link.getAttribute("href");
+
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+});
